5159cc4d90df8392536867c1a2ccec1d8f283767
PazAutumn <pazcontrerastello@gmail.com>
2018-02-06 13:46:32 -0300

    primer commit

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+var options = {
+	enableHighAccuracy: true,
+	timeout: 6000,
+	maximunAge: 0
+};
+
+navigator.geolocation.getCurrentPosition( success, error, options );
+
+function success(position) {
+	var coordenadas = position.coords;
+
+	console.log('Tu posición actual es:');
+	console.log('Latitud : ' + coordenadas.latitude);
+	console.log('Longitud : ' + coordenadas.longitude);
+	console.log('Más o menos ' + coordenadas.accuracy + ' metros.');
+};
+
+function error(error) {
+	console.warn('ERROR(' + error.code + '): ' + error.message);
+};
