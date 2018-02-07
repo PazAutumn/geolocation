8e0a517c2d03c1cc411c65a89631b2aba6860077
PazAutumn <pazcontrerastello@gmail.com>
2018-02-06 22:55:53 -0300

    ubicación

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,96 @@
+function initMap() {
+    var laboratoriaLima = {
+        lat: -12.1191427,
+        lng: -77.0349046
+    };
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 18,
+        center: laboratoriaLima
+    });
+
+    var markadorLaboratoria = new google.maps.Marker({
+        position: laboratoriaLima,
+        map: map
+    });
+}
+
+document.getElementById('encuentrame').addEventListener('click', buscar);
+
+function buscar() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+}
+
+var latitude, longitude;
+var success = function(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+
+    var miUbicacion = new google.maps.Marker({
+        position: ({
+            lat: latitude,
+            lng: longitude
+        }),
+        setMap: map
+    });
+
+    /*
+    map.setZoom(18),
+    map.setCenter({
+        lat: latitude,
+        lng: longitude
+    })
+    */
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 18,
+        center: ({
+            lat: latitude,
+            lng: longitude
+        })
+    })
+}
+
+var error = function(error) {
+    alert('Tenemos un problema con encontrar tu ubicación');
+}
+
+
+
+
+
+
+/*
+$(document).ready(function() {
+
+});
+
+$('#findme').click(function() {
+    if (navigator.geolocation) {
+        alert('Tu navegador soporta geolocalización');
+    } else {
+        alert('Tu navegador no soporta geolocalización')
+    }
+
+    function localización(position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+
+        alert('tu latitud es: ' + latitude + ', tu longitud es: ' + longitude);
+    }
+
+    function error(error) {
+        console.warn('ERROR(' + error.code + '): ' + error.message);
+    };
+
+    navigator.geolocation.getCurrentPosition(localización, error);
+});
+
+
+
+
 
 var options = {
 	enableHighAccuracy: true,
@@ -21,10 +114,11 @@ function error(error) {
 };
 
 
+
 function initMap() {
     var currentPosition = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
+        lat: -33.421673999999996,
+        lng: -70.6882608
     };
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 4,
@@ -34,4 +128,5 @@ function initMap() {
         position: currentPosition,
         map: map
     });
-}
+};
+*/
